ccfaa5e7f3e64b1c2894b08206f027629d736edf
Zied Ben Amor <zied-ci@hotmail.fr>
2022-09-13 16:37:53 +0200

    fix image path issue and close icon

--- js/projectData.js
+++ js/projectData.js
@@ -1,7 +1,34 @@
 const data = [
   {
     title: 'Keeping track of hundreds of components',
-    src: '../images/Snapshoot Portfolio.jpg',
+    src: './images/Snapshoot Portfolio.jpg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
+    live: 'https://zied2112.github.io/MicroProfile/',
+    source: 'https://github.com/zied2112/MicroProfile',
+  },
+  {
+    title: 'Keeping track of hundreds of components',
+    src: './images/Snapshoot Portfolio.jpg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
+    live: 'https://zied2112.github.io/MicroProfile/',
+    source: 'https://github.com/zied2112/MicroProfile',
+  },
+  {
+    title: 'Keeping track of hundreds of components',
+    src: './images/Snapshoot Portfolio.jpg',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
+    live: 'https://zied2112.github.io/MicroProfile/',
+    source: 'https://github.com/zied2112/MicroProfile',
+  },
+  {
+    title: 'Keeping track of hundreds of components',
+    src: './images/Snapshoot Portfolio.jpg',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
@@ -19,34 +46,7 @@ const data = [
   },
   {
     title: 'Keeping track of hundreds of components',
-    src: '../images/Snapshoot Portfolio.jpg',
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
-    live: 'https://zied2112.github.io/MicroProfile/',
-    source: 'https://github.com/zied2112/MicroProfile',
-  },
-  {
-    title: 'Keeping track of hundreds of components',
-    src: '../images/Snapshoot Portfolio.jpg',
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
-    live: 'https://zied2112.github.io/MicroProfile/',
-    source: 'https://github.com/zied2112/MicroProfile',
-  },
-  {
-    title: 'Keeping track of hundreds of components',
-    src: '../images/Snapshoot Portfolio.jpg',
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
-    live: 'https://zied2112.github.io/MicroProfile/',
-    source: 'https://github.com/zied2112/MicroProfile',
-  },
-  {
-    title: 'Keeping track of hundreds of components',
-    src: '../images/Snapshoot Portfolio.jpg',
+    src: './images/Snapshoot Portfolio.jpg',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
     lang: ['Ruby on rails', 'HTML', 'css', 'javascript'],
