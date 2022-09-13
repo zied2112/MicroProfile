9bf5d1b7e972a7b2fd1548634e0b5c764ebfa8e7
blase147 <solarmails2@gmail.com>
2022-09-13 15:52:34 +0100

    Fix linters errors

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
-const menuBtn = document.getElementById("menu-btn");
-const closeBtn = document.getElementById("close-btn");
-const menu = document.getElementById("menu");
-const projectContainer = document.getElementById("projects-container");
+import data from './projectData.js';
 
-const popup = document.getElementById("popup");
-import data from "./projectData.js";
-console.log(data);
-menuBtn.addEventListener("click", () => {
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const menu = document.getElementById('menu');
+const projectContainer = document.getElementById('projects-container');
+
+const popup = document.getElementById('popup');
+menuBtn.addEventListener('click', () => {
   // console.log('Clicked');
-  menu.classList.add("menu-show");
-  closeBtn.style.display = "block";
+  menu.classList.add('menu-show');
+  closeBtn.style.display = 'block';
 });
-closeBtn.addEventListener("click", () => {
-  menu.classList.remove("menu-show");
-  closeBtn.style.display = "none";
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('menu-show');
+  closeBtn.style.display = 'none';
 });
-menu.addEventListener("click", () => {
-  menu.classList.remove("menu-show");
-  closeBtn.style.display = "none";
+menu.addEventListener('click', () => {
+  menu.classList.remove('menu-show');
+  closeBtn.style.display = 'none';
 });
 
 data.forEach((data) => {
-  const { title, src, description, lang, live, source } = data;
-  const project = document.createElement("div");
+  const {
+    title, src, lang,
+  } = data;
+  const project = document.createElement('div');
   project.innerHTML += `<div class="project">
           <div class="project-image">
           <img src="${src}">
@@ -30,57 +32,55 @@ data.forEach((data) => {
           <div class="project-description">
             <h3>${title}</h3>
             <ul>
-              ${lang.map((tech) => `<li>${tech}</li>`).join("")}
+              ${lang.map((tech) => `<li>${tech}</li>`).join('')}
             </ul>
             <button class="project-btn btn">See Project</button>
           </div>
         </div>`;
   projectContainer.appendChild(project);
-  popup.style.display = "none";
-  var projectBtn = project.querySelector(".project-btn");
-  projectBtn.addEventListener("click", () => {
-    console.log(data);
-
-    document.getElementById("popup").scrollIntoView();
-    projectContainer.style.display = "none";
-    popup.style.display = "flex";
+  popup.style.display = 'none';
+  const projectBtn = project.querySelector('.project-btn');
+  projectBtn.addEventListener('click', () => {
+    document.getElementById('popup').scrollIntoView();
+    projectContainer.style.display = 'none';
+    popup.style.display = 'flex';
     const y = popup.getBoundingClientRect().top + window.scrollY;
     window.scroll({
       top: y,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
     popup.innerHTML += `<div class="popup-project-container">
     <div class="popup-project-header">
     <div class="popup-project-image"><img src="${
-      data.src
-    }" class="popup-img" alt="projectimage"></div>
+  data.src
+}" class="popup-img" alt="projectimage"></div>
     <button class="popup-close-button">
     <i class="fa-solid fa-xmark"></i>
     </button>
         <h2 class="popup-project-title">${data.title}</h2>
         <ul>
-        ${lang.map((tech) => `<li>${tech}</li>`).join("")}
+        ${lang.map((tech) => `<li>${tech}</li>`).join('')}
         </ul>
         </div>
         <div class="popup-project-description">${data.description}</div>
         <div class="popup-project-buttons">
         <a href="${
-          data.live
-        }" target="_blank"><button class="popup-project-btn">
+  data.live
+}" target="_blank"><button class="popup-project-btn">
         See Live<i class="fa-solid fa-globe"></i>
         </button></a>
         <a href="${
-          data.source
-        }" target="_blank"><button class="popup-project-btn">
+  data.source
+}" target="_blank"><button class="popup-project-btn">
         See Source<i class="fa-brands fa-github"></i>
         </button></a>
         </div>
         </div>`;
-    var closeProjectBtn = popup.querySelector(".popup-close-button");
-    closeProjectBtn.addEventListener("click", () => {
+    const closeProjectBtn = popup.querySelector('.popup-close-button');
+    closeProjectBtn.addEventListener('click', () => {
       popup.firstElementChild.remove();
-      popup.style.display = "none";
-      projectContainer.style.display = "grid";
+      popup.style.display = 'none';
+      projectContainer.style.display = 'grid';
     });
   });
 });
